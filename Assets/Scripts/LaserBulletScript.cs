16383b71638607d99f479682a1e7b4b64570a50d
Oscar Garcia Toledo <narcyz2048@gmail.com>
2020-05-25 18:20:14 +0200

    Laser hit effects

--- Assets/Scripts/LaserBulletScript.cs
+++ Assets/Scripts/LaserBulletScript.cs
@@ -9,7 +9,7 @@ public class LaserBulletScript : MonoBehaviour
     
     private Vector3 direction;
     private CapsuleCollider collider;
-    public VisualEffect sparks;
+    public VisualEffect sparkEffect;
     [Header("Projectile Parameters")]
     public ParticleSystem explosionEffect;
     public float lifetime = 3;
@@ -33,20 +33,29 @@ public class LaserBulletScript : MonoBehaviour
         // transform.position += direction * speed * Time.deltaTime;
     }
     void OnTriggerEnter(Collider other){
+        // 
+        
         if(other.tag == "Solid"){
-            print(other.name);
-            sparks.Reinit();
-            sparks.Play();
-            Destroy(this);
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, transform.forward, out hit))
-            {
-                Debug.Log("Point of contact: "+hit.point);
-                Debug.Log(hit.normal);
-                Debug.DrawLine(hit.point,hit.point+hit.normal*2,Color.red,4);
-                sparks.transform.rotation = Quaternion.LookRotation(hit.normal);
-                
-            }
+            
+            // print(other.name);
+            // if(sparkEffect != null){
+                VisualEffect sparks = Instantiate(sparkEffect);
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, transform.forward, out hit))
+                {
+                    Debug.Log("Point of contact: "+hit.point);
+                    Debug.Log(hit.normal);
+                    Debug.DrawLine(hit.point,hit.point+hit.normal*2,Color.red,4);
+                    Debug.DrawRay(transform.position, transform.forward, Color.green);
+                    sparks.transform.position = hit.point;
+                    sparks.transform.rotation = Quaternion.LookRotation(hit.normal);
+                    
+                }
+            // }
+            
+            Destroy(this.gameObject);
+
+            
         }
     }
 }
