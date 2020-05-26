90ae683175ff8ae68aa303048f0cd94ec52d219e
Oscar Garcia Toledo <narcyz2048@gmail.com>
2020-05-26 17:29:05 +0200

    Simple Health Bar

---
+++ Assets/Scripts/UiHandlerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class UiHandlerScript : MonoBehaviour
+{
+
+    public Image HealthBarPercent;
+    [Range(0.0f, 1.0f)]
+    public float percent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HealthBarPercent.fillAmount = percent;
+        HealthBarPercent.color = new Color(1-percent,percent,0);
+    }
+}
